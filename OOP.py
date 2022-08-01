1b6f191a12a8177e9bbbc85b5b15edf19d4b7bd3
Иван <kamenev2003kamenev@gmail.com>
2022-08-01 17:34:03 +0700

    da, da da

--- OOP.py
+++ OOP.py
@@ -3,18 +3,20 @@ class Building:
     pass
 
 class HouseBuilding(Building):
-    num_of_pepople = 0
+    
+    def __init__(self, stairs, num_of_people):
+        self.stairs = stairs
+        self.num_of_people = num_of_people    
 
-    def info(self):
-       print(f'Number of stairs in the building: {Building2.stairs}; Number of householders in the building: {Building2.num_of_pepople}') 
+    def __str__(self):
+       print(f'Number of stairs in the building: {Building2.stairs}; Number of householders in the building: {Building2.num_of_people}') 
 
 Building1 = Building()
-Building2 = HouseBuilding()
+Building2 = HouseBuilding(21, 1000)
 
 Building1.stairs = 3
-Building2.stairs = 21
-Building2.num_of_pepople = 1000
 
-Building2.info()
+Building2.__str__()
 
 print("Hellow")
+
