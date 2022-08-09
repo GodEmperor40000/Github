3572a642f0a96daedef9e9ce5d95f0b4d5a53d24
Иван <kamenev2003kamenev@gmail.com>
2022-08-09 17:50:30 +0700

    some OOP stuff

--- OOP.py
+++ OOP.py
@@ -1,20 +1,56 @@
+def openDoor(num_of_people):
+        print(f'door was opened {num_of_people} times')
+
 class Building:
     stairs = 0
     pass
 
 class HouseBuilding(Building):
-    num_of_pepople = 0
+
+    def __init__(self, num_of_appartments, stairs):
+        self.stairs = stairs
+        self.num_of_appartments = num_of_appartments
 
     def info(self):
-       print(f'Number of stairs in the building: {Building2.stairs}; Number of householders in the building: {Building2.num_of_pepople}') 
+       print(f'Number of stairs in the building: {self.stairs}; Number of appartments in the building: {self.num_of_appartments}') 
+
+    def enterBuilding(self,num_of_people):
+        openDoor(num_of_people)
+        print(f'{num_of_people} people are in the House Building')
+
+class OfficeBuilding(Building):
+    
+    def __init__(self, num_of_offices, stairs):
+        self.stairs = stairs
+        self.num_of_offices = num_of_offices
+
+    def info(self):
+        print(f'Number of stairs in the Office building: {self.stairs}; Number of offices in the building: {self.num_of_offices}')
+
+    def enterBuilding(self, num_of_people):
+        openDoor(num_of_people)
+        print(f'{num_of_people} people are working in the Office building')
 
 Building1 = Building()
-Building2 = HouseBuilding()
+Building2 = HouseBuilding(200, 40)
+Building3 = OfficeBuilding(1000, 50)
 
-Building1.stairs = 3
-Building2.stairs = 21
-Building2.num_of_pepople = 1000
+num_of_HouseBuilding = 4
+num_of_OfficeBuilding = 3
 
-Building2.info()
+for_Houses = [(10,1), (100,4), (5, 2), (500, 20)]
+for_Offices = [(3, 3), (2, 1), (40, 10)]
 
-print("Hellow")
+HouseBuildings = [HouseBuilding(for_Houses[a][0], for_Houses[a][1]) for a in range(num_of_HouseBuilding)]
+OfficeBuildings = [OfficeBuilding(for_Offices[a][0], for_Offices[a][1]) for a in range(num_of_OfficeBuilding)]
+
+buildings = []
+buildings.extend(HouseBuildings)
+buildings.extend(OfficeBuildings)
+
+for building in buildings:
+    building.info()
+
+
+
+
